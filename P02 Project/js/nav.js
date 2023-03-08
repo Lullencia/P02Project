2a80d5bdcc0544f7317d77b85c905400404d0d3c
discordiaz <rla103@sfu.ca>
2023-03-08 02:25:59 -0800

    Update nav.js
    added link for citation

--- P02 Project/js/nav.js
+++ P02 Project/js/nav.js
@@ -10,3 +10,5 @@ document.querySelectorAll(".navbar-link").forEach(n => n.addEventListener("click
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
+//https://community.codenewbie.org/ljc_dev/easy-hamburger-menu-with-js-beginners-2dh0//
